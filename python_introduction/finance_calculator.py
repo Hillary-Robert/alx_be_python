48db3ff000fcb66874a731b5297c5f768c05eb70
Hillary Robert <roberthillary30@gmail.com>
2025-09-09 17:21:19 +0300

    usead autograder specific float method and variable name

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,12 +1,11 @@
-userSalary = int(input("Enter your monthly income: "))
-userExpenses = int(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-userSavings = userSalary - userExpenses
+monthly_savings = monthly_income - monthly_expenses
 
-rate = 5/100
+rate = 5 / 100
 
-projectedSavings = userSavings * 12 + (userSavings * 12 * rate)
+projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * rate)
 
-print(f"Your monthly savings are ${userSavings}.")
-
-print(f"Projected savings after one year, with interest, is: ${projectedSavings}.")
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
