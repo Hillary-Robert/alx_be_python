c0a60fae015eedf301ebd9c2d0f46dab31704295
Hillary Robert <roberthillary30@gmail.com>
2025-09-20 15:43:07 +0300

    Added control flow if conditions

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,11 @@
-userResponse = str(input("What is the weather like today? (sunny/rainy/cold): "))
+userResponse = str(input("What's the weather like today? (sunny/rainy/cold): "))
 userResponseLow = userResponse.lower()
 
-if (userResponseLow == "sunny"):
-  print("Wear a t-shirt and sunglasses.")
-elif(userResponseLow == "rainy"):
-  print("Don't forget your umbrella and a raincoat.")
-elif(userResponseLow == "cold"):
-  print("Make sure to wear a warm coat and a scarf.")
+if userResponseLow == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif userResponseLow == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif userResponseLow == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-  print("Sorry, I don't have recommendations for this weather.")
-
-
+    print("Sorry, I don't have recommendations for this weather.")
