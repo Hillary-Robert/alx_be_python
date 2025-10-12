384a7d6f43b08a598e669a5986d290546cd9f4be
Hillary Robert <roberthillary30@gmail.com>
2025-10-12 18:23:48 +0300

    Exploring Polymorphism and Method Overriding

--- oop/main.py
+++ oop/main.py
@@ -1,17 +1,35 @@
-from book_class import Book
+# from book_class import Book
 
-def main():
+# def main():
     
-    my_book = Book("1984", "George Orwell", 1949)
+#     my_book = Book("1984", "George Orwell", 1949)
 
     
-    print(my_book) 
+#     print(my_book) 
 
    
-    print(repr(my_book))  
+#     print(repr(my_book))  
 
     
-    del my_book
+#     del my_book
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+from polymorphism_demo import Shape, Rectangle, Circle
+import math
+
+def main():
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
+
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
 
 if __name__ == "__main__":
     main()
